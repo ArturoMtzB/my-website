35092a9ccd52ca4d3215fed0eb02a88f571eb1ec
Arturo Mtz <46832169+ArturoMtzB@users.noreply.github.com>
2023-01-09 15:45:58 -0600

    Complete development of Header component (nav bar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import "./App.css"
 import Header from './components/header/Header'
+import Home from './components/home/Home'
 
-const App = () => (
+const App = () => {
+    return (
         <>
-            <Header/>
+            <Header />
+            <Home />
         </>
      )
+}
 
 export default App
